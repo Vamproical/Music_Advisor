a2b40403d682e452caf02fc2012404813ca2352a
Vamproical <mimoun136@yandex.ru>
2020-07-10 12:32:41 +0900

    Complete stage 1.

--- Music Advisor/task/src/advisor/Main.java
+++ Music Advisor/task/src/advisor/Main.java
@@ -1,7 +1,45 @@
 package advisor;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (!input.equals("exit")) {
+            input = scanner.nextLine();
+            switch (input) {
+                case "new":
+                    System.out.println("---NEW RELEASES---\n" +
+                            "Mountains [Sia, Diplo, Labrinth]\n" +
+                            "Runaway [Lil Peep]\n" +
+                            "The Greatest Show [Panic! At The Disco]\n" +
+                            "All Out Life [Slipknot]");
+                    break;
+                case "categories":
+                    System.out.println("---CATEGORIES---\n" +
+                            "Top Lists\n" +
+                            "Pop\n" +
+                            "Mood\n" +
+                            "Latin\n");
+                    break;
+                case "featured":
+                    System.out.println("---FEATURED---\n" + "Mellow Morning\n" +
+                            "Wake Up and Smell the Coffee\n" +
+                            "Monday Motivation\n" +
+                            "Songs to Sing in the Shower\n");
+                    break;
+                case "playlists Mood":
+                    System.out.println("---MOOD PLAYLISTS---\n" +
+                            "Walk Like A Badass  \n" +
+                            "Rage Beats  \n" +
+                            "Arab Mood Booster  \n" +
+                            "Sunday Stroll");
+                    break;
+                case "exit":
+                    System.out.println("---GOODBYE!---");
+                    break;
+            }
+        }
     }
 }
