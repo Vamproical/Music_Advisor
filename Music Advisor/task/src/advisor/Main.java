1b6d9e517e8d5178db80e6a64335bb18730a637d
Vamproical <mimoun136@yandex.ru>
2020-07-10 13:02:32 +0900

    Complete stage 2.

--- Music Advisor/task/src/advisor/Main.java
+++ Music Advisor/task/src/advisor/Main.java
@@ -5,16 +5,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean isAuth = false;
         String input = "";
         while (!input.equals("exit")) {
             input = scanner.nextLine();
             switch (input) {
+                case "auth":
+                    System.out.println("https://accounts.spotify.com/authorize?client_id=c915d91261d3470a8a920b25c8687a04redirect_uri=http://localhost:8080&response_type=code");
+                    System.out.println();
+                    System.out.println("---SUCCESS---\n");
+                    isAuth = true;
+                    break;
                 case "new":
-                    System.out.println("---NEW RELEASES---\n" +
-                            "Mountains [Sia, Diplo, Labrinth]\n" +
-                            "Runaway [Lil Peep]\n" +
-                            "The Greatest Show [Panic! At The Disco]\n" +
-                            "All Out Life [Slipknot]");
+                    if (!isAuth) {
+                        System.out.println("Please, provide access for application.");
+                    } else {
+                        System.out.println("---NEW RELEASES---\n" +
+                                "Mountains [Sia, Diplo, Labrinth]\n" +
+                                "Runaway [Lil Peep]\n" +
+                                "The Greatest Show [Panic! At The Disco]\n" +
+                                "All Out Life [Slipknot]");
+                    }
                     break;
                 case "categories":
                     System.out.println("---CATEGORIES---\n" +
@@ -24,10 +35,14 @@ public class Main {
                             "Latin\n");
                     break;
                 case "featured":
-                    System.out.println("---FEATURED---\n" + "Mellow Morning\n" +
-                            "Wake Up and Smell the Coffee\n" +
-                            "Monday Motivation\n" +
-                            "Songs to Sing in the Shower\n");
+                    if (!isAuth) {
+                        System.out.println("Please, provide access for application.");
+                    } else {
+                        System.out.println("---FEATURED---\n" + "Mellow Morning\n" +
+                                "Wake Up and Smell the Coffee\n" +
+                                "Monday Motivation\n" +
+                                "Songs to Sing in the Shower\n");
+                    }
                     break;
                 case "playlists Mood":
                     System.out.println("---MOOD PLAYLISTS---\n" +
